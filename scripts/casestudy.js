e7247b9896880c50955fed92d700465140aaa889
Mira <mira927@gmail.com>
2020-06-14 21:07:57 -0500

    clean up based on reviewer comments

--- scripts/casestudy.js
+++ scripts/casestudy.js
@@ -1,10 +1,14 @@
+// Check distance to top and display back-to-top.
+$( window ).scroll( function() {
+	if ( $( this ).scrollTop() > 800 ) {
+		$( '.back-to-top' ).addClass( 'show-back-to-top' );
+	} else {
+		$( '.back-to-top' ).removeClass( 'show-back-to-top' );
+	}
+});
 
-
-//alert("Connected!");
-
-
-
-function buttonClick() {
-  // your code here
-  alert("Clicked");
-}
+// Click event to scroll to top.
+$( '.back-to-top' ).click( function() {
+	$( 'html, body' ).animate( { scrollTop : 0 }, 800 );
+	return false;
+});
